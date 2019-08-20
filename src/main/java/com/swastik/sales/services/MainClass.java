06b28493784a074f45cb2102443d17fdf08c2f59
shubhamsrstvpersonal email = shubham.srstv@gmail.com <shubham.srstv@gmail.com>
2019-08-20 23:11:53 +0530

    Adding one annotation

--- src/main/java/com/swastik/sales/services/MainClass.java
+++ src/main/java/com/swastik/sales/services/MainClass.java
@@ -1,10 +1,12 @@
 package com.swastik.sales.services;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 @CrossOrigin
 public class MainClass {
 
